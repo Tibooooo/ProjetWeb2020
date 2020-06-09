490f41729c1186b603beb1ce2fddb5c61dbe3965
Tibooooo <50449880+Tibooooo@users.noreply.github.com>
2020-06-09 09:18:12 +0200

    Add files via upload

---
+++ prj-cir2-web-front/php/classements.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Classements</title>
+        <link type="text/css" rel="stylesheet" href="css/main.css">
+
+    </head>
+    <body>
+        <?php 
+            include('header.php');
+        ?>
+        <div id="contenu">
+            <h1>Classements</h1>
+        </div>
+
+
+
+
+        <?php
+            include('footer.php');
+        ?>
+
+    </body>
+</html>
